feb5132a89f35c7ed73f66922af15bcf04d03214
abdelrahmannagy1 <abdelrahmannagy1@gmail.com>
2023-03-06 10:44:30 +0200

    added index on start_time in reservations

--- TableMangement/models.py
+++ TableMangement/models.py
@@ -6,6 +6,6 @@ class Table(models.Model):
     
 
 class Reservation(models.Model):
-    start_time = models.DateTimeField() 
+    start_time = models.DateTimeField(db_index=True) 
     end_time = models.DateTimeField()
     table = models.ForeignKey('TableMangement.Table', on_delete=models.PROTECT, null=True)
